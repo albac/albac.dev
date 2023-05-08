176a0182796de8c4940ac102a6ad24bd4778abc2
Alfredo Baldoceda <alfredo@ripbullnetworks.com>
2023-05-08 18:36:20 -0400

    responsivenes for 2xl

--- components/BlogListItem.js
+++ components/BlogListItem.js
@@ -18,7 +18,7 @@ function BlogListItem(item) {
         Published {format(parseISO(item.createdAt), "MMMM do, uuu")} <br/>
         Last updated on {format(parseISO(item.updatedAt), "MMMM do, uuu")}
       </div>
-      <div className="italic text-base 2xl:text-xl xl:text-md lg:text-md">{item.summary}</div>
+      <div className="italic text-base 2xl:text-lg xl:text-md lg:text-md">{item.summary}</div>
     </div>
   );
 }
