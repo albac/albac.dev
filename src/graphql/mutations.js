5d0627eb1c00ccdb359c475c2993af2feab669c7
Alfredo Baldoceda <alfredo@ripbullnetworks.com>
2023-05-07 22:38:56 -0400

    beta changes and stage env changes

---
+++ src/graphql/mutations.js
@@ -0,0 +1,60 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createPosts = /* GraphQL */ `
+  mutation CreatePosts(
+    $input: CreatePostsInput!
+    $condition: ModelPostsConditionInput
+  ) {
+    createPosts(input: $input, condition: $condition) {
+      id
+      title
+      content
+      summary
+      state
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+    }
+  }
+`;
+export const updatePosts = /* GraphQL */ `
+  mutation UpdatePosts(
+    $input: UpdatePostsInput!
+    $condition: ModelPostsConditionInput
+  ) {
+    updatePosts(input: $input, condition: $condition) {
+      id
+      title
+      content
+      summary
+      state
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+    }
+  }
+`;
+export const deletePosts = /* GraphQL */ `
+  mutation DeletePosts(
+    $input: DeletePostsInput!
+    $condition: ModelPostsConditionInput
+  ) {
+    deletePosts(input: $input, condition: $condition) {
+      id
+      title
+      content
+      summary
+      state
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+    }
+  }
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,51 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreatePosts = /* GraphQL */ `
+  subscription OnCreatePosts($filter: ModelSubscriptionPostsFilterInput) {
+    onCreatePosts(filter: $filter) {
+      id
+      title
+      content
+      summary
+      state
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+    }
+  }
+`;
+export const onUpdatePosts = /* GraphQL */ `
+  subscription OnUpdatePosts($filter: ModelSubscriptionPostsFilterInput) {
+    onUpdatePosts(filter: $filter) {
+      id
+      title
+      content
+      summary
+      state
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+    }
+  }
+`;
+export const onDeletePosts = /* GraphQL */ `
+  subscription OnDeletePosts($filter: ModelSubscriptionPostsFilterInput) {
+    onDeletePosts(filter: $filter) {
+      id
+      title
+      content
+      summary
+      state
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+    }
+  }
+`;
