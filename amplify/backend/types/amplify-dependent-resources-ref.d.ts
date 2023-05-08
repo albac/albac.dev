d684f23b60734205c27d7dddba300979dd4692e8
Alfredo Baldoceda <alfredo@ripbullnetworks.com>
2023-05-08 12:19:57 -0400

    adding permissions

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -22,6 +22,10 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "AdminsGroupRole": "string",
+      "EditorsGroupRole": "string"
     }
   },
   "function": {
