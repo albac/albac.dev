502af5477ce91dc304f731611b997287fbd320dc
Alfredo Baldoceda <alfredo@ripbullnetworks.com>
2023-05-11 17:35:59 -0400

    upgrading image to support nextjs 13

--- components/LogoTitle.js
+++ components/LogoTitle.js
@@ -8,25 +8,21 @@ function LogoTitle({ title }) {
     <div className="flex flex-row text-3xl py-2 lg:py-1 xl:py-2 pl-1 sm:pl-2">
       <Link href="/">
         <div
-          className="hover:cursor-pointer"
+          className="hover:cursor-pointer 2xl:w-20 2xl:h-20 lg:w-14 lg:h-14 md:w-14 md:h-14 sm:w-12 sm:h-12 w-16 h-16"
           style={{
             position: "relative",
-            width: "68px",
-            height: "68px",
           }}
         >
           <h1>
             <Image
               alt="albac"
               src={LogoAlbac}
-              layout="fill"
-              objectFit="cover"
             />
           </h1>
         </div>
       </Link>
       <h2>
-        <div className="pl-5 pt-2 sm:pt-4 pr-2 font-thin font-sans text-sm sm:text-2xl lg:text-xl xl:pt-4 lg:pt-2">
+        <div className="pl-5 pt-2 sm:pt-4 pr-2 font-thin font-sans text-xl sm:text-xl lg:text-xl 2xl:text-2xl xl:pt-4 lg:pt-2">
           {title}
         </div>
       </h2>
