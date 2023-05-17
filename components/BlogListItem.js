acf746b18998a0d01102ab93321e8bd5809c876d
isaul19 <saulhola348@gmail.com>
2023-05-17 16:41:23 -0500

    ajuste del tamaño de los titulos

--- components/BlogListItem.js
+++ components/BlogListItem.js
@@ -9,7 +9,7 @@ function BlogListItem(item) {
     <div className="border font-light 2xl:space-y-6 space-y-2 dark:hover:border-slate-400 bg-slate-100/75 dark:bg-slate-800/75 dark:border-white/5 hover:border-white border-gray-400 rounded p-4 sm:p-8 lg:p-10 2xl:p-12 lg:px-12 xl:px-16 2xl:px-20">
       <div>
         <Link href={`/blog/${slug}`}>
-          <button className="italic text-lg lg:text-xl 2xl:text-2xl text-rose-800 hover:text-rose-700 dark:hover:text-sky-400 dark:text-sky-600 font-semibold antialiased">
+          <button className="italic text-lg sm:text-[22px] 2xl:text-[26px] text-rose-800 hover:text-rose-700 dark:hover:text-sky-400 dark:text-sky-600 font-semibold antialiased">
             {item.title}
           </button>
         </Link>
