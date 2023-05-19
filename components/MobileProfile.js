f066b60f16960a19016bbaa5ff4f4347bbf96003
isaul19 <saulhola348@gmail.com>
2023-05-19 17:01:28 -0500

    restoure 'mobile-profile'

--- components/MobileProfile.js
+++ components/MobileProfile.js
@@ -1,16 +1,14 @@
 import ImageS3 from "./ImageS3";
 
-function LargeProfile() {
+export default function MobileProfile() {
   return (
-        <div className="xl:mt-2 lg:mt-2 mt-28">
-          <ImageS3
+      <div className="px-16 mt-1 py-4">
+        <ImageS3
             src="albac_summer1_mobile.png"
             alt="albac"
             className="rounded-full overflow-hidden sm:px-22 px-8 py-6 w-[434px] "
             priority={true}
           />
-        </div>
+      </div>
   );
 }
-
-export default LargeProfile;
