2f55cc1b1e60f68570171340365e619415ad9d57
isaul19 <saulhola348@gmail.com>
2023-05-19 15:46:48 -0500

    change to ImageS3 in 'mobile profile'

--- components/MobileProfile.js
+++ components/MobileProfile.js
@@ -1,17 +1,16 @@
-import Image from "next/image";
-import ProfilePic from "../public/albac_summer1_mobile.png";
+import ImageS3 from "./ImageS3";
 
-export default function MobileProfile() {
+function LargeProfile() {
   return (
-    <>
-      <div className="px-16 mt-1 py-4">
-        <Image
-          src={ProfilePic}
-          alt="albac"
-          className="rounded-full overflow-hidden sm:px-22 px-8 py-6"
-          priority={true}
-        />
-      </div>
-    </>
+        <div className="xl:mt-2 lg:mt-2 mt-28">
+          <ImageS3
+            src="albac_summer1_mobile.png"
+            alt="albac"
+            className="rounded-full overflow-hidden sm:px-22 px-8 py-6 w-[434px] "
+            priority={true}
+          />
+        </div>
   );
 }
+
+export default LargeProfile;
