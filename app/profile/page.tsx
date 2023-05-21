e1b48019a60e30f36a0a6b86d3e62aea5fc0d9ff
isaul19 <saulhola348@gmail.com>
2023-05-21 16:08:09 -0500

    added metadata in 'profile' and 'projects' pages

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,9 @@
+export const metadata = {
+  title: 'Albac Dev Profile Bio',
+  description: 'Bio and background',
+  keywords: ['AWS', 'Portfolio', 'Bio'],
+};
+
 export default function ProfilePage() {
-  return (
-    <div>Profile Page</div>
-  )
+  return <div>Profile Page</div>;
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,19 @@
+export const metadata = {
+  title: 'AlbacDev: Projects',
+  description: 'Blogs posted by AlbacDev',
+  keywords: [
+    'Aws',
+    'amplify',
+    's3',
+    'cognito',
+    'dynamodb',
+    'tailwind',
+    'nextjs',
+    'javascript',
+    'react',
+  ],
+};
+
 export default function ProjectsPage() {
-  return (
-    <div>Projects Page</div>
-  )
+  return <div>Projects Page</div>;
 }
