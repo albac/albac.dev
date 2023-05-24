5a3777ae5a52c0870b04ff7678f8dfc78c8e726d
Alfredo Baldoceda <alfrds@gmail.com>
2023-05-24 10:57:57 -0400

    Adding client component

--- app/profile-img.tsx
+++ app/profile-img.tsx
@@ -4,18 +4,20 @@ import { Amplify } from 'aws-amplify';
 import awsExports from "../src/aws-exports";
 
 import Image from 'next/image';
-import ImageS3Url from '../utils/ImageS3Url';
 
 Amplify.configure({ ...awsExports, ssr: true });
 
 export default async function ProfileImage() {
-  const alfredoImg = await ImageS3Url('albac_summer1_desktop.webp');
+
+  const imageURL = await Storage.get('albac_summer1_desktop.webp', {
+    level: "public",
+  });
 
   return (
         <div className="w-[65%] md:w-[50%] lg:w-[35%]">
           <Image
             className="mx-auto w-[70%] rounded-full lg:max-w-[200px] lg:scale-125"
-            src={alfredoImg}
+            src={imageURL}
             alt="alfredo-img"
             height={200}
             width={200}
