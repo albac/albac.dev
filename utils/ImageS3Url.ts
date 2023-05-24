ac1d3faea92e38d43aaa1cdb8068b65aa1b62ea8
Alfredo Baldoceda <alfrds@gmail.com>
2023-05-23 21:31:37 -0400

    Testing server side image with Amplify config

--- utils/ImageS3Url.ts
+++ utils/ImageS3Url.ts
@@ -1,9 +1,15 @@
-import { Storage } from 'aws-amplify';
+import { Amplify, withSSRContext, Storage } from "aws-amplify";
+import awsExports from "../src/aws-exports";
+
+Amplify.configure({ ...awsExports, ssr: true });
+
 
 export default async function ImageS3Url(name: string) {
+
   const imageURL = await Storage.get(name, {
-    level: 'public',
+    level: "public",
   });
 
   return imageURL;
+
 }
