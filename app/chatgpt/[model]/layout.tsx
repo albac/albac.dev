06e7f091d59a2f390d961c9e075448cf42983eb9
isaul19 <saulhola348@gmail.com>
2023-05-25 10:34:54 -0500

    add metadata in chatgpt page

---
+++ app/chatgpt/[model]/layout.tsx
@@ -0,0 +1,26 @@
+export const metadata = {
+  title: "Albac.Dev - ChatGPT API Interfaces using OpenAI API",
+  description:
+    "ChatGPT is a specific implementation of GPT designed for interactive chatting and conversation. It utilizes natural language processing (NLP) and machine learning techniques to understand and generate human-like responses to user input.",
+  keyboard: [
+    "react",
+    "chatgpt",
+    "openai",
+    "next.js",
+    "serverless",
+    "gpt",
+    "api",
+    "nlp",
+    "aws",
+    "amplify",
+    "developer",
+    "devops",
+    "fullstack",
+    "alfredo",
+    "alfredo baldoceda",
+  ],
+};
+
+export default function Layout({ children }) {
+  return { children };
+}
