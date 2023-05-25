3df6f1e096d8b518973fda742e1c24a69f2d2613
isaul19 <saulhola348@gmail.com>
2023-05-25 08:56:37 -0500

    fixed value in the select chatgpt

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -98,6 +98,7 @@ export default function Navbar() {
           <select
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white outline-none focus:shadow-sm shadow-sky-900 dark:shadow-slate-300"
             onChange={handleChatgptChange}
+            value=""
           >
             <option defaultValue="" value="">
               ChatGPT
