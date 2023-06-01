144cd8be5839ca9ea7726a4d064b13bb2bf4794e
isaul19 <saulhola348@gmail.com>
2023-06-01 17:58:01 -0500

    limit openai api

--- amplify/backend/function/davinciFunction/src/index.py
+++ amplify/backend/function/davinciFunction/src/index.py
@@ -3,51 +3,45 @@ import openai
 import boto3
 import os
 
+# CONFIG ---------------------
 ENCRYPTED = os.environ['OPENAI_KEY']
-
 ssm = boto3.client('ssm')
 
-# PROPS
-PROMPT="now you can only answer questions related to technology, programming languages, cloud services such as aws or azure, if you are asked questions from another topic just answer that you can not answer I will repeat this message in each question, here goes the question, apply what I told you and answer in the language that this the question:"
+# PROPS ----------------------
+MODEL_LIMIT="now you can only answer questions related to technology, programming languages, cloud services such as aws or azure, if you are asked questions from another topic just answer that you can not answer I will repeat this message in each question, here goes the question, apply what I told you and answer in the language that this the question:"
 MAX_TOKENS=2048
 
-def handler(event, context):
-
-    data = json.loads(event["body"])
-    
-    if len(data["prompt"]) > MAX_TOKENS:
-        data["text"] = f"The query exceeds the token limit allowed ({MAX_TOKENS}). Please shorten your query."
-        return {
-            "statusCode": 400,
+def buildResponse(status, text):
+    response = [];
+    response["text"] = text
+    return {
+            "statusCode": status,
             "headers": {
                 "Access-Control-Allow-Headers": "*",
                 "Access-Control-Allow-Origin": "*",
                 "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
-            },
-            "body": json.dumps(data),
+            },  
+            "body": json.dumps(response),
         }
+
+# LAMBDA -----------------------
+def handler(event, context):
+    data = json.loads(event["body"])
+    
+    if len(data["prompt"]) > MAX_TOKENS:
+        return buildResponse(400, f"The query exceeds the token limit allowed ({MAX_TOKENS}). Please shorten your query.")
         
     openai.api_key = ssm.get_parameter(
             Name=ENCRYPTED, WithDecryption=True)['Parameter']['Value']
 
-    response = openai.Completion.create(
+    openai_data = openai.Completion.create(
         model="text-davinci-003",
-        prompt=f"{PROMPT} '{data['prompt']}'",
+        prompt=f"{MODEL_LIMIT} '{data['prompt']}'",
         max_tokens=MAX_TOKENS,
         temperature=0.9,
         frequency_penalty=0.5,
         presence_penalty=1.0
     )
 
-    text = response['choices'][0]['text']
-    data['text'] = text
-
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Access-Control-Allow-Headers": "*",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
-        },
-        "body": json.dumps(data),
-    }
+    text = openai_data['choices'][0]['text']
+    return buildResponse(200, text)
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,42 +5,44 @@ import "../styles/globals.css";
 import AuthenticatorProvider from "../components/AuthenticatorProvider";
 
 interface RootLayoutProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 if (process.env.USER_BRANCH === "prod") {
-    awsconfig.oauth.redirectSignIn = "https://albac.dev/";
-    awsconfig.oauth.redirectSignOut = "https://albac.dev/";
+  awsconfig.oauth.redirectSignIn = "https://albac.dev/";
+  awsconfig.oauth.redirectSignOut = "https://albac.dev/";
 } else if (process.env.USER_BRANCH === "stage") {
-    awsconfig.oauth.redirectSignIn = "https://beta.albac.dev/";
-    awsconfig.oauth.redirectSignOut = "https://beta.albac.dev/";
+  awsconfig.oauth.redirectSignIn = "https://beta.albac.dev/";
+  awsconfig.oauth.redirectSignOut = "https://beta.albac.dev/";
 } else {
-    awsconfig.oauth.redirectSignIn = "http://localhost:3000/";
-    awsconfig.oauth.redirectSignOut = "http://localhost:3000/";
+  awsconfig.oauth.redirectSignIn = "http://localhost:3000/";
+  awsconfig.oauth.redirectSignOut = "http://localhost:3000/";
 }
 
 Amplify.configure({
-    ...awsconfig,
-    ssr: true,
-    DataStore: {
-        authModeStrategyType: AuthModeStrategyType.MULTI_AUTH,
-    },
+  ...awsconfig,
+  ssr: true,
+  DataStore: {
+    authModeStrategyType: AuthModeStrategyType.MULTI_AUTH,
+  },
 });
 
 export default async function RootLayout({ children }: RootLayoutProps) {
-    return (
-        <html lang="en">
-            <body className="h-screen dark:text-slate-300 bg-slate-100 dark:bg-slate-900 px-5 md:px-[3vw]">
-                <AuthenticatorProvider>
-                    <header className="pt-5">
-                        <Navbar />
-                    </header>
-                    {children}
-                    <footer className="text-center text-slate-600 dark:text-slate-400 p-4">
-                        <p>All rights reserved &copy; albac.dev {new Date().getFullYear()} </p>
-                    </footer>
-                </AuthenticatorProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body className="h-screen dark:text-slate-300 bg-slate-100 dark:bg-slate-900 px-5 md:px-[3vw]">
+        <AuthenticatorProvider>
+          <header className="pt-5">
+            <Navbar />
+          </header>
+          {children}
+          <footer className="text-center text-slate-600 dark:text-slate-400 p-4">
+            <p>
+              All rights reserved &copy; albac.dev {new Date().getFullYear()}{" "}
+            </p>
+          </footer>
+        </AuthenticatorProvider>
+      </body>
+    </html>
+  );
 }
