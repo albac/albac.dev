ac6f703c0534d2adb6582ad2608c3936ad35a892
Alfredo Baldoceda <alfrds@gmail.com>
2023-12-04 23:27:16 -0500

    fixing deployment

--- src/ui-components/HeroLayout1.jsx
+++ src/ui-components/HeroLayout1.jsx
@@ -10,13 +10,12 @@ import {
   getOverrideProps,
   getOverridesFromVariants,
   mergeVariantsAndOverrides,
-} from "@aws-amplify/ui-react/internal";
+} from "./utils";
 import { Button, Flex, Image, Text } from "@aws-amplify/ui-react";
 export default function HeroLayout1(props) {
   const { overrides: overridesProp, ...rest } = props;
   const variants = [
     {
-      variantValues: { mode: "Light" },
       overrides: {
         "LOREM IPSUM": {},
         "Ut enim ad minim veniam quis nostrud": {},
@@ -30,9 +29,9 @@ export default function HeroLayout1(props) {
         Right: {},
         HeroLayout1: {},
       },
+      variantValues: { mode: "Light" },
     },
     {
-      variantValues: { mode: "Dark" },
       overrides: {
         "LOREM IPSUM": { color: "rgba(255,255,255,1)" },
         "Ut enim ad minim veniam quis nostrud": {
@@ -48,6 +47,7 @@ export default function HeroLayout1(props) {
         Right: {},
         HeroLayout1: {},
       },
+      variantValues: { mode: "Dark" },
     },
   ];
   const overrides = mergeVariantsAndOverrides(
